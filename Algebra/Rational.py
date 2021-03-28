800843b2dd2d31f1a0c957a210871e3dd3abea29
Dusty <lacid305123@bk.ru>
2021-03-28 12:36:01 +0300

    Rational __init_ can take no arguments now

--- Algebra/Rational.py
+++ Algebra/Rational.py
@@ -6,9 +6,18 @@ __all__ = ["Rational"]
 
 class Rational():
 
-    def __init__(self, n, m):
-        self._numerator = Integer(n)
-        self._denumerator = Natural(m)
+    def __init__(self, n = None, m = None):
+        # Если в __init__ не был передан ни один аргумент
+        # то создаём "пустое" число
+        if n is None and m is None:
+            self._numerator = Integer()
+            self._denumerator = Natural()
+        # Если m не было введено
+        # то оно считается равным единице
+        else:
+            self._numerator = Integer(n)
+            self._denumerator = Integer("1" if m is None else m)
+
 
     def __str__(self):
         return str(self._numerator) + " / " + str(self._denumerator)
