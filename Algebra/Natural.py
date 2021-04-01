68d6b53535cac1082d68ce46a4e03f63f39899f9
IceFroggit <zeksengali0@gmail.com>
2021-04-01 11:54:30 +0300

    # Conflicts: #	Algebra/Natural.py

--- Algebra/Natural.py
+++ Algebra/Natural.py
@@ -20,7 +20,7 @@ class Natural():
             self._dig_n = 1
         else:
             # Убираем все нули
-            while n.find("0") != -1:
+            while n.find("0") != -1 and n[0] == "0":
                 n = remove_char(n, n.index("0"))
             self._number = [int(i) for i in n[::-1]]
             self._dig_n = len(self._number)
@@ -35,6 +35,42 @@ class Natural():
         else:
             return "0"
 
+    '''Модуль N-6.MUL_ND_N-mul_d #6 выполнил и оформил Цыганков Дмитрий'''
+    def mul_d(self,digit:int):
+        self_c = Natural(str(self))
+        if self_c._dig_n != 0: #если числа не пустые
+            if self_c._number == [0] or digit == 0: #если одно из чисел нулевое
+                return Natural("0")
+            else:
+                temp = 0 #остаток после умножения разряда на число.
+                for j in range(self_c._dig_n):
+                    if temp != 0: #если после предыдущего шага есть остаток
+                        if self_c._number[j]*digit + temp >= 10: #если умножение очередной цифры числа оказалось больше 10
+                            temp_n = self_c._number[j]
+                            self_c._number[j] = (self_c._number[j]*digit + temp)%10
+                            temp = (temp_n*digit + temp)//10
+                        else:
+                            self_c._number[j] = self_c._number[j]*digit + temp
+                            temp = 0
+                    elif self_c._number[j]*digit >= 10:#если умножение очередной цифры числа оказалось больше 10
+                        temp_n = self_c._number[j]
+                        self_c._number[j] = (self_c._number[j]*digit)%10
+                        temp = (temp_n*digit)//10
+                    else:
+                        self_c._number[j] = self_c._number[j]*digit
+                        temp = 0
+                if temp != 0: #если на последнем шаге остался остаток, то записываем его в 1 позицию
+                    self_c._number.insert(self_c._dig_n, temp)
+                return self_c
+        else:
+            return Natural()
+
+    '''Модуль N-7.MUL_Nk_N-mul_k #11 выполнил и оформил Цыганков Дмитрий'''
+    def mul_k(self,tenpow:int):
+        self_c = Natural(str(self))
+        for i in range(tenpow):
+            self_c._number.insert(i,0)
+        return self_c
 
     def __gt__(self, num):
         '''Модуль переполнения ">". Оформил Шабров Иван'''
@@ -50,6 +86,17 @@ class Natural():
                     return False
         return False
 
+
+    def __eq__(self, num):
+        '''Перегрузка оператора "==". Оформила Реброва Юлия'''
+
+        if self._dig_n != num._dig_n:
+            return False
+        else:
+            for i in range(num._dig_n):
+                if self._number[i] != num._number[i]:
+                    return False
+            return True
 
 
     def __lt__(self, num):
@@ -75,9 +122,16 @@ class Natural():
         else:
             return 0
 
+    def is_zero(self):
+        '''Модуль NZER_N_B. Оформила Реброва Юлия'''
+        if self == Natural("0"):
+            return True
+        else:
+            return False
+
     def __add__(self, num):
-        '''Модуль ADD_NN_N. Оформил Крысиль Жексенгалиев'''
-        #Сложение натуральных чисел
+        '''Модуль ADD_NN_N. Оформил Адиль Жексенгалиев'''
+        # Сложение натуральных чисел
         res = Natural()
         if self.compare(num) == 1:
             res._dig_n = num._dig_n
@@ -111,8 +165,6 @@ class Natural():
 
             i = i + 1
         # Для случая когда числа имеют разные разряды
-        # Для случая когда числа имеют разные разряды
-        # Для случая когда числа имеют разные разряды
         j = 0
         while j < abs(self._dig_n - num._dig_n):
             # cтоит ли заменить метод  на переменную или оставить так для читаемости кода?
@@ -123,7 +175,7 @@ class Natural():
             if res._number[j + i] == 10:
                 res._number[j + i] = 0
                 if (j + i == res._dig_n - 1):
-                    res._dig_n+=1
+                    res._dig_n += 1
                     res._number.append(0)
 
                 res._number[j + i + 1] += 1
@@ -137,14 +189,6 @@ class Natural():
         for i in range(x._dig_n):
             s += mul_k(mul_d(self, x._number[i]), i)
         return s
-
-    # def icm (self,num):
-    # '''Модуль LCM_NN_N. Оформил Жексенгалиев Адиль'''
-    # NoD = gcf(self, num)
-    # res = self * num
-    # while res > NoD:
-    #   res = res - NoD
-    # return res
 
     def __sub__(self, num):
         '''Модуль LCM_NN_N. Оформил Жексенгалиев Адиль'''
@@ -199,4 +243,3 @@ class Natural():
             del res._number[i]
             i -= 1
         return res
-
